a8940d86e1a99a9825b98fc55ea5b4ed45d2a742
Jessica Alba <jessica.alba@morningstar.com>
2025-09-02 13:50:56 -0500

    feat: lising 2.4

--- hello/main_internal_test.go
+++ hello/main_internal_test.go
@@ -1,7 +1,14 @@
 package main
 
-func Example_main() {
-	main()
-	// Output:
-	// Hello, World!
+import "testing"
+
+func TestGreet( t *testing.T) {
+	want := "Hello, World!"
+
+	got := greet()
+
+	if got != want {
+		// mark this test as failed
+		t.Errorf("expected: %q, got: %q", want, got)
+	}
 }
