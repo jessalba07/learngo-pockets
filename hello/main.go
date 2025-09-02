3267e53db0fc67178750dd5ea26fc148b3d856af
Jessica Alba <jessica.alba@morningstar.com>
2025-09-02 13:44:41 -0500

    feat: listing 2.3

--- hello/main.go
+++ hello/main.go
@@ -2,5 +2,11 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!");
+    greeting := greet()
+
+    fmt.Println(greeting)
+}
+
+func greet() string {
+    return "Hello, World!"
 }
